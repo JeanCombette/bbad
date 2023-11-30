d1551b9a3aa9664d0bf17a4c7c4feb3b471ce902
JeanCombette <jean.combette@etu.unilim.fr>
2023-11-30 16:56:26 +0100

    test3

--- my-project2/src/App.jsx
+++ my-project2/src/App.jsx
@@ -4,17 +4,12 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 
 export default function App() {
-
-
-  
-
   
   return (
     <>
       <Step1/>
       <Step2/>
       <Step3/>
-
 
     </>
   );
